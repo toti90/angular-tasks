6084df65cf66927ca4a5e2f4422c84ea6e73d6e9
toti90 <tothtomi7@gmail.com>
2022-05-03 16:16:52 +0200

    base components

--- angularTasks/src/app/app.module.ts
+++ angularTasks/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { AnswerComponent } from './answer/answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule,
